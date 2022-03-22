4143a8f9da853e35623ba5acdb0e8a8ab6937199
Guilherme <acousg1@gmail.com>
2022-03-22 20:36:47 -0300

    att

---
+++ script.js
@@ -0,0 +1,10 @@
+function clickMenu(){
+    var nav = document.getElementById('nav')
+    nav.classList.toggle('active')
+}
+//menu-hamburguer
+
+function toTop(){
+    scrollTo(0, 0) //animar o back to top
+}//back-to-top
+
